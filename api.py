867d87ca9cec4c8d82ce122316598c0b327d5823
Yajat <yajatapps3@gmail.com>
2021-04-18 20:42:14 -0700

    Hash passwords before adding to database

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 import os
 from fastapi import FastAPI
+import hashlib
 import mysql.connector
 
 app = FastAPI()
@@ -19,15 +20,23 @@ async def home():
 
 @app.post('/api/v1/messenger/users/add')
 async def add_user(user: str = None, pwd: str = None):
+    # Add a new user to UsersAuth database with passed username and sha256 encrypted password
+
     if user == None or pwd == None:
         return 'user or pwd missing'
 
     appCursor = appDb.cursor()
 
-    sql = "INSERT INTO UsersAuth (Username, Password) VALUES (%s, %s)"
-    val = (user, pwd)
+    sql = "INSERT INTO UsersAuth (username, password) VALUES (%s, %s)"
+    val = (user, sha256(pwd))
     appCursor.execute(sql, val)
 
     appDb.commit()
 
     return 'added'
+
+
+def sha256(hash: str):
+    # Util function to return sha256 hash of the passed argument
+
+    return hashlib.sha256(hash.encode()).hexdigest()
